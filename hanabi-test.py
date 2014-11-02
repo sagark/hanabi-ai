c0d4635649adf1e88a16efb00a79e8462f0b9b00
Julia Schwarz <julenka@cs.cmu.edu>
2014-11-02 16:22:13 -0500

    Adding logic to play cards on table.

---
+++ hanabi-test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+''' Implements some very basic tests I ran during development. 
+
+
+Note these are not unit tests as there is currently no way to control specifics of game state.
+
+'''
+
+from hanabi import *
+
+def testPlayCard():
+	''' Tests whether players can play cards on the table '''
+	print "testPlayCard start"
+	game = Game(3)
+	# test playing to a deck
+	cur = game.players[game.cur_player]
+	playable_cards = cur.getPlayableCards(game.table)
+	while len(playable_cards) > 0:
+		cur.play(playable_cards[0], game.table)
+		game.show()
+		game.cur_player += 1
+		game.cur_player %= len(game.players)
+		cur = game.players[game.cur_player]
+		playable_cards = cur.getPlayableCards(game.table)
+	print "testPlayCard end"
+	
+def testShowGame():
+	game = Game(3)
+	game.show()
--- hanabi.py
+++ hanabi.py
@@ -2,7 +2,13 @@
 
 """ Simulates a game of hanabi, allowing for experimentation of different hanabi strategies assuming perfect memory """
 
+from collections import defaultdict
 import random
+import logging 
+
+logging.basicConfig()
+logger = logging.getLogger('hanabi')
+logger.setLevel(logging.DEBUG)
 
 # Components
 # Deck
@@ -16,6 +22,9 @@ HANABI_COLORS = ["red", "blue", "green", "yellow", "white"]
 HANABI_SUIT = [1,1,1,2,2,3,3,4,4,5]
 # If there are 2 or 3 players, each player receives 5 cards. If there are 4 or 5 players, each player receives 4 cards.
 CARDS_PER_PLAYER = {2:5,3:5,4:4,5:4}
+
+INITIAL_NUM_CLOCK_TOKENS = 8
+INITIAL_NUM_FUSE_TOKENS = 4
 
 class Card:
 	""" A card in Hanabi 
@@ -79,16 +88,58 @@ class Deck:
 
 class Player:
 	""" A player in Hanabi """
-	def __init__(self, cards):
+	def __init__(self, cards, name):
 		# cards in hand
 		self.cards = cards
 		# guesses about cards
 		self.guesses = [Guess() for c in self.cards]
+		self.name = name
+
+	def play(self, i, t):
+		''' Play a card at index i on table t'''
+		logger.debug("{} playing {}".format(self.name, self.cards[i]))
+		t.playCard(self.cards[i])
+		del self.cards[i]
+
+	def getPlayableCards(self, table):
+		''' Return the indices of all playable card in this hand '''
+		return [i for i,card in enumerate(self.cards) if table.canPlayCard(card)]
+
+	def drawCard(self, deck):
+		self.cards.append(deck.drawCard())
 
 	def __str__(self):
 		# on each line, print the actual card and guess for that card
 		lines = ["{}\t{}".format(card, guess) for card, guess in zip(self.cards, self.guesses) ]
+		print self.name
 		return "\n".join(lines)
+
+class Table:
+	""" Represents the game table, e.g. the area where cards are played """
+	def __init__(self, data=None):
+		# data is a map from color -> cards
+		if data:
+			self.data = data
+		else:
+			self.data = defaultdict(list)
+
+	def playCard(self, card):
+		""" Plays a card on the table, assuming it is valid.
+
+		If not valid, throw exception
+		"""
+		if not self.canPlayCard(card):
+			raise Exception("playCard invalid card: {}, current table is {}".format(card, self))
+		self.data[card.color].append(card)
+
+	def canPlayCard(self, card):
+		color = card.color
+		number = card.number
+		num_cards_in_color = len(self.data[color])
+		return num_cards_in_color + 1 == number
+
+	def __repr__(self):
+		return "Table(data={})".format(self.data)
 
 class Game:
 	def __init__(self, n_players):
@@ -101,8 +152,17 @@ class Game:
 			raise Exception ("Num players must be between 2 and 5")
 		self.deck = Deck()
 		self.players = []
-		for _ in xrange(n_players):
-			self.players.append(Player(self.deck.drawCards(CARDS_PER_PLAYER[n_players])))
+
+		# Game is over is num fuse or num clock tokens goes below zero
+		self.num_fuse_tokens = INITIAL_NUM_FUSE_TOKENS
+		self.num_clock_tokens = INITIAL_NUM_CLOCK_TOKENS
+
+		self.cur_player = 0
+		self.discard_pile = []
+		self.table = Table()
+
+		for i in xrange(n_players):
+			self.players.append(Player(self.deck.drawCards(CARDS_PER_PLAYER[n_players]), "player {}".format(i)))
 
 	def printHeader(self, header):
 		separator = "*" * 40
@@ -112,13 +172,22 @@ class Game:
 		print separator
 
 	def show(self):
+		self.printHeader("BEGIN GAME STATE")
 		self.printHeader("Players")
-		for i,p in enumerate(self.players):
-			print "Player {}".format(i)
+		for p in self.players:
 			print str(p)
 
 		self.printHeader("Deck")
-		print str(self.deck)
+		print self.deck
 
-game = Game(3)
-game.show()
+		self.printHeader("Table")
+		print self.table
+
+		print
+		print "cur_player: {}".format(self.cur_player)
+		print "discard pile: {}".format(self.discard_pile)
+		print "num_fuse_tokens: {}".format(self.num_fuse_tokens)
+		print "num_clock_tokens: {}".format(self.num_clock_tokens)
+		self.printHeader("END GAME STATE")
+		print
+
