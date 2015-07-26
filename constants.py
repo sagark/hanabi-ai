32a19f4fa57220831ec3f51f0a6ee3f4b3ef292b
Julia Schwarz <julenka@cs.cmu.edu>
2015-07-26 08:40:46 -0400

    cleanup

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@
 
 # Globals specific to game
 HANABI_COLORS = ["red", "blue", "green", "yellow", "white"]
-HANABI_SUIT = [1,1,1,2,2,3,3,4,4,5]
+HANABI_SUIT = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5]
 N_CARDS_PER_SUIT = len(set(HANABI_SUIT))
 # If there are 2 or 3 players, each player receives 5 cards. If there are 4 or 5 players, each player receives 4 cards.
 CARDS_PER_PLAYER = {2:5,3:5,4:4,5:4}
